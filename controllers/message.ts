9eb59a1e2b2b5ec4fffcb4a3f2478b0ea0955d27
AriPaez <paezariel69@gmail.com>
2023-10-16 20:03:38 -0300

    create conection api whatsapp

--- controllers/message.ts
+++ controllers/message.ts
@@ -4,28 +4,36 @@ dotenv.config();
 
 
 export const verifyToken=(req:Request,res:Response)=>{
-
-    let accessToken:string="";
-    res.json({
-        msg:'verifyToken'
-    })
-    
+ 
     try 
     {
-        accessToken=process.env.ACCES_TOKEN_FB || "";
-        console.log(req.query["hub.verify_token"]);
-    } 
-    catch (error)
-    {
         
+        let accessToken=process.env.ACCES_TOKEN_FB;
+       
+        let token =req.query["hub.verify_token"];
+ 
+        console.log("Estoy pasando por aqui!");
+        let challange =req.query["hub.challenge"];
+ 
+        if(challange!=null && token!=null && token==accessToken)
+        {
+            res.send(challange);
+            
+        }
+        else
+        {
+            res.status(400).send();
+             
+        }
+    } 
+    catch (error) {
+        res.status(400).send();
     }
-
+         
 }
 
 export  const reciveMessage=(req:Request,res:Response)=>{
-    res.json({
-        msg:'reciveMessage'
-    })
+    
 
 }
  
--- models/server.ts
+++ models/server.ts
@@ -1,7 +1,8 @@
 import express,{Application} from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 dotenv.config();
+import cors from 'cors';
+
 
 import useRoutes from '../routes/message';
 
